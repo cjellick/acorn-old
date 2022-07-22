ce5f99789ac1a4796b6ed653f7387bd2430880e2
Craig Jellick <craig@acorn.io>
2022-07-22 12:13:34 -0700

    make acornDnsEndpoint resettable

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -36,7 +36,7 @@ func complete(c *apiv1.Config, ctx context.Context, getter kclient.Reader) error
 	if c.AcornDNS == nil {
 		c.AcornDNS = &[]string{"auto"}[0]
 	}
-	if c.AcornDNSEndpoint == nil {
+	if c.AcornDNSEndpoint == nil || *c.AcornDNSEndpoint == "" {
 		c.AcornDNSEndpoint = &AcornDNSEndpointDefault
 	}
 
